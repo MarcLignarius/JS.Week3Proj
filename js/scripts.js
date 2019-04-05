d17df42e44959a6e1aacf701d0f30eb827e33fb0
Marc Davies <marcdaviesriot@gmail.com>
2019-04-05 10:24:16 -0700

    programs splits the numbers counted into their own element

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 // Business logic
+
+var exception1 = [1];
+var exception2 = [2];
+var exception3 = [3];
+
 function count(userNumber) {
   var result = "";
   for (var i = 0; i <= userNumber; i++) {
@@ -12,7 +17,7 @@ $(document).ready(function() {
     event.preventDefault();
     var userNumber = parseInt($("input#userNumber").val());
     console.log("userNumber", userNumber);
-    var output = count(userNumber)
+    var output = count(userNumber).split("");
     console.log("output", output);
     $("#output").text(output);
   });
