22b83a2bb18447b22b83af75fc9b674cb56f23b8
Marc Davies <marcdaviesriot@gmail.com>
2019-04-05 09:36:43 -0700

    program takes a number from the user and returns it in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$(function() {
+// Business logic
 
+// User Interface Logic
+$(document).ready(function() {
+  $("form#userInput").submit(function(event) {
+    event.preventDefault();
+    var userNumber = parseInt($("input#userNumber").val());
+    console.log(userNumber);
+    $("#output").text(userNumber);
+  });
 });
