b36ddf3d95ff5e179fe87f397dfbe259fe6b3040
Marc Davies <marcdaviesriot@gmail.com>
2019-04-05 09:49:23 -0700

    program takes number from user and counts up to and including that number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 // Business logic
-
+function count(userNumber) {
+  var result = "";
+  for (var i = 0; i <= userNumber; i++) {
+    result += i;
+  }
+  return result;
+};
 // User Interface Logic
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("input#userNumber").val());
-    console.log(userNumber);
-    $("#output").text(userNumber);
+    console.log("userNumber", userNumber);
+    var output = count(userNumber)
+    console.log("output", output);
+    $("#output").text(output);
   });
 });
